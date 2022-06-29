3108f0229acfb8d3480aff6a441cf67e230f5d14
AntonAboimov <aboimov13@gmail.com>
2022-06-29 16:54:28 +0300

    Add TOP-1_rent.sql

---
+++ 3. TOP-1_rent.sql
@@ -0,0 +1,19 @@
+--Вывод таблицы ТОП1 по одному из 3-х параметров. 
+
+select *
+from (select
+       	store_id
+       	, date_trunc('day', payment_date)::date as sale_date
+       	, rank () over (partition by store_id order by day_count desc) as rank_max_count
+       	, day_count
+       	, rank () over (partition by store_id order by day_sum) as rank_min_sum
+       	, day_sum
+ 	from
+	        	(select *
+                     	, count (payment_id) over (partition by store_id, date_trunc('day', payment_date)) as day_count
+                     	, sum (amount) over (partition by store_id, date_trunc('day', payment_date)) as day_sum
+	        	from payment p
+	        	join store s2 on p.staff_id = s2.manager_staff_id) as t2
+ 	group by store_id, sale_date, day_count, day_sum
+ 	) t3
+where rank_max_count = 1 or rank_min_sum = 1
